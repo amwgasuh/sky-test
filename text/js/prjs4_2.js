32ee4cdb91ea762160566d1f9e838eb31f96860b
amwgasuh <1299950497@qq.com>
2017-10-28 11:47:29 +0800

    new

---
+++ text/js/prjs4_2.js
@@ -0,0 +1,76 @@
+//var num1 = 2;
+//	result = ++num1;
+//alert("num1= " + num1 + "result=" + result);
+//num1 = 2;
+//result = num1++;
+//alert("num1= " + num1 + " result= " + result);
+
+/*************************************************/
+
+//var paycheck = 2000;
+//alert(paycheck += paycheck);
+//alert(paycheck -=500);
+//alert(paycheck -=paycheck);
+//alert(paycheck +=500);
+//alert(paycheck -=80);
+
+/*************************************************/
+
+//var havecookbook = "yes";
+//	meatloafrecipe = "yes";
+//if (havecookbook === "yes") {
+//	if (meatloafrecipe === "yes") {
+//		window.alert("Recipe found");
+//	}
+//	else {
+//		window.alert("Have the book but no recipe");
+//	}
+//}
+//else {
+//	window.alert("You need a cookbook");
+//}
+
+/*************************************************/
+
+//var username = window.prompt("Give me your name!","");
+//if ( (username === null ) || (username === "" ) ) {
+//	document.write("Hello,Random Nameless Person!");
+//}
+//else {
+//	document.write("Hello, " + username + "!");	
+//}
+
+/*************************************************/
+
+//var msg = "",
+//	city = window.prompt("Whit is he name of your city?");
+//if ( (city === null) || (city ==="" ) ) {
+//	msg = "No city enterd. Cannot determine delivery availabilite.";
+//}
+//else {
+//	switch (city) {
+//		case "Johnstown" :
+//			msg = "We can have items delivered to you in 3days.";
+//			break;
+//		case "Donville" :
+//			msg = "We can have items delivered to you in 1 week";
+//			break;
+//		case " Daineltown || Maryville " :
+//			msg = "We can have items delivered to you in 2 weeks";
+//			break;
+//		default :
+//		msg = "Sorry!We do not deliver to your city yet.";
+//	}
+//}
+//document.write(msg);
+
+/*************************************************************/
+
+var student0 = "thomas",
+	student1 = "Roger",
+	student2 = "Amber",
+	student3 = "Jennifer";
+document.write("student0 + <br />");
+document.write("student1 + <br />");
+document.write("student2 + <br />");
+document.write("student3 + <br />");
